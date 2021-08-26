9365e169ad2a40ec3692cd01e7e0e5dc85575eeb
dlwo12345 <fire@dumy.co.kr>
2021-08-26 10:29:27 +0900

    라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,20 @@
-import { useEffect, useRef } from 'react';
-import 'cropperjs/dist/cropper.css';
-import Cropper from 'cropperjs';
-import './test.css';
-import testImg from './test.jpg';
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import ImageEditComponent from './pages/ImageEdit/ImageEditComponent'
 
-let cropper: Cropper;
+
 function App() {
-  const imageRef = useRef<HTMLImageElement | null>(null);
-
-  useEffect(() => {
-    const minAspectRatio = 0.5;
-    const maxAspectRatio = 1.5;
-    if (imageRef?.current) {
-      cropper = new Cropper(imageRef.current!, {
-        ready: () => {
-          const containerData = cropper.getContainerData();
-          const cropBoxData = cropper.getCropBoxData();
-          const aspectRatio = cropBoxData.width / cropBoxData.height;
-          let newCropBoxWidth;
-
-          if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio) {
-            newCropBoxWidth =
-              cropBoxData.height * ((minAspectRatio + maxAspectRatio) / 2);
-
-            cropper.setCropBoxData({
-              left: (containerData.width - newCropBoxWidth) / 2,
-              width: newCropBoxWidth,
-            });
-          }
-        },
-
-        cropmove: () => {
-          const cropBoxData = cropper.getCropBoxData();
-          const aspectRatio = cropBoxData.width / cropBoxData.height;
-
-          if (aspectRatio < minAspectRatio) {
-            cropper.setCropBoxData({
-              width: cropBoxData.height * minAspectRatio,
-            });
-          } else if (aspectRatio > maxAspectRatio) {
-            cropper.setCropBoxData({
-              width: cropBoxData.height * maxAspectRatio,
-            });
-          }
-        },
-      });
-    }
-
-
-
-  }, [])
-
   return (
-    <div className="container">
-      <h1>Cropper with a range of aspect ratio</h1>
-      <div>
-        <img id="image" ref={imageRef} src={testImg} alt="aaapicture" />
-      </div>
-    </div>
+    <BrowserRouter>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path={`/image`} component={ImageEditComponent} />
+          <Route exact path={`/`}>
+            <Redirect to="/image" />
+          </Route>
+        </Switch>
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
