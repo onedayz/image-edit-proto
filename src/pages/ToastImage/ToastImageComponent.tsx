6f4fc927bb5a37f6bb28db0e66bd25644944181d
dlwo12345 <fire@dumy.co.kr>
2021-09-02 02:00:10 +0900

    .

--- src/pages/ToastImage/ToastImageComponent.tsx
+++ src/pages/ToastImage/ToastImageComponent.tsx
@@ -3,21 +3,36 @@ import 'tui-image-editor/dist/tui-image-editor.css';
 // @ts-ignore
 import ImageEditor from '@toast-ui/react-image-editor';
 import { useRef } from 'react';
+import testImg from '../../test.jpg';
+import { Button } from 'antd';
 
 
 function ToastImageComponent() {
     const editorRef = useRef<any>();
 
+    const handleCick = () => {
+        const el = editorRef.current.getInstance();
+
+        console.log('el', el);
+        el.setDrawingShape('rect', {
+            fill: {
+                type: 'filter',
+                filter: [{ blur: 0.3 }, { pixelate: 20 }]
+            },
+            width: 100,
+            height: 200
+        });
+    }
     return (
         <>
             <ImageEditor
                 ref={editorRef}
                 includeUI={{
                     loadImage: {
-                        path: 'img/sampleImage.jpg',
+                        path: testImg,
                         name: 'SampleImage',
                     },
-                    menu: ['shape', 'filter'],
+                    menu: ['crop', 'flip', 'rotate', 'shape', 'icon', 'text', 'mask', 'filter'],
                     initMenu: 'filter',
                     uiSize: {
                         width: '1000px',
@@ -33,6 +48,7 @@ function ToastImageComponent() {
                 }}
                 usageStatistics={true}
             />
+            <Button onClick={handleCick}>123</Button>
         </>
     );
 }
