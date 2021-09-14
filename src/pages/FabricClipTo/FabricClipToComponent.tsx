f6982204d753b3b8b1f06b8c3c188696920d2ec8
dlwo12345 <fire@dumy.co.kr>
2021-09-14 21:44:58 +0900

    .

--- src/pages/FabricClipTo/FabricClipToComponent.tsx
+++ src/pages/FabricClipTo/FabricClipToComponent.tsx
@@ -13,12 +13,26 @@ const getBase64 = (file: any) => {
 
 const FabricClipToComponent: React.FC = () => {
     const [canvas, setCanvas] = useState<fabric.Canvas | undefined>();
+    const [clipPath, setClipPath] = useState<fabric.Object | undefined>();
     const initCanvas = () => {
         const canvas: any = new fabric.Canvas('canvas', {
             height: 800,
             width: 800,
+            controlsAboveOverlay: true, // clipPath 영역밖 컨트롤 표시여부
+            backgroundColor: '#fff'
         })
-        return canvas;
+        const clipPath = new fabric.Rect({
+            width: 400,
+            height: 400,
+            left: 200,
+            top: 200,
+            stroke: "rgba(0, 0, 0, 0.5)",
+            strokeWidth: 1,
+        });
+
+        canvas.clipPath = clipPath;
+        setCanvas(canvas);
+        setClipPath(clipPath);
     };
 
     const imageUpload = async (e: any) => {
@@ -55,6 +69,7 @@ const FabricClipToComponent: React.FC = () => {
                         scaleY: 360 / imgElement.height
                     });
                     canvas!.add(imgInstance);
+                    canvas!.centerObject(imgInstance); // object center 이동
                 }
 
             } catch (e) {
@@ -66,18 +81,47 @@ const FabricClipToComponent: React.FC = () => {
     }
 
     const download = () => {
-        const aTag = document.createElement('a');
-        aTag.download = 'from_canvas.png';
-        aTag.href = canvas!.toDataURL({
-            format: "png",
-            quality: 0.8,
+        const c = document.createElement("canvas");
+        c.width = 1280;
+        c.height = 1280;
+        const ctx: any = c.getContext("2d");
+        let cropped = new Image();
+        cropped.src = canvas!.toDataURL({
+            left: clipPath!.left,
+            top: clipPath!.top,
+            width: clipPath!.width,
+            height: clipPath!.height,
+            withoutShadow: true,
         });
-        console.log('aTag.href', aTag.href);
-        aTag.click();
+
+        cropped.onload = function () {
+            ctx.drawImage(cropped, 0, 0, 1280, 1280);
+            const img = c.toDataURL("image/jpg");
+            const aTag = document.createElement('a');
+
+            aTag.download = 'from_canvas.jpg';
+
+            aTag.href = img;
+
+            aTag.click();
+        };
+
+        // const aTag = document.createElement('a');
+        // aTag.download = 'from_canvas.png';
+        // aTag.href = canvas!.toDataURL({
+        //     format: "png",
+        //     quality: 0.8,
+        //     left: clipPath!.left,
+        //     top: clipPath!.top,
+        //     width: clipPath!.width,
+        //     height: clipPath!.height,
+        // });
+        // console.log('aTag.href', aTag.href);
+        // aTag.click();
     }
 
     useEffect(() => {
-        setCanvas(initCanvas());
+        initCanvas();
     }, []);
 
     return (
@@ -89,7 +133,7 @@ const FabricClipToComponent: React.FC = () => {
                 name='imageFile'
                 onChange={imageUpload} />
             <div>
-                <canvas id="canvas" style={{ border: "1px solid gray" }} />
+                <canvas id="canvas" />
             </div>
             <button onClick={download}>다운로드</button>
             <button onClick={() => {
